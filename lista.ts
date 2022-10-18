06a0e7ca89353cf8384056246f01eee2c421bb6e
Gabriel Rodrigo <pdktche-pb@live.com>
2022-10-17 21:25:24 -0300

    Foi feito os exercicios 1 e 2.

---
+++ lista.ts
@@ -0,0 +1,42 @@
+//Ex 1
+// let numeros:number[]
+// let par:number
+// par = 2%0
+// let impar:number
+// impar = 3%0
+// numeros = [1,2,3,-4,-5]
+// numeros.forEach(numero =>{
+//     if(numero == par ){
+//         numero = 1
+//     }
+//     else{
+//         numero = -1
+//     }
+// })
+// console.log(numeros)
+
+
+//Ex.2
+// let array:string[]
+// let index:number
+// let numero:string 
+// function InserirNoArray(){
+//     for (let index = 0; index < 5; index++) {
+//         numero = prompt("Digite um numero:")!
+//         array.push(numero)
+//         if(array[index] == numero){
+//             alert("Você já digitou este numero,por favor digite outro.")
+//         }
+//     }
+// }
+
+//Ex 3
+let arrayA: number[] = [1,2,6,9,2,3,5]
+let arrayB: number[] = [1,3,5,6,8,1,2]
+let arrayC: number[]
+let continuar: boolean = false
+
+while(continuar){
+let inserirNumero: number = prompt("Digite um numero para inserir no array.")!
+arrayA.push(inserirNumero)
+}
